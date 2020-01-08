e1dd3f00d93afe082860edb5e42a9a7f63ac7cde
Ennyola <medunoyeeni@gmail.com>
2020-01-08 18:54:26 +0100

    requirement file removed

--- buycoinsTest/buycoinsTest/settings.py
+++ buycoinsTest/buycoinsTest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h=j=5jxc790qjyn2^k0#c45#bulcfpd!v^@=3g7iob*ikh1(zf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
